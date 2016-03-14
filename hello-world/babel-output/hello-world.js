87cc52ef1eaccd08b0700f2ff11c78e71aca50c6
cebial <biglazypanda@gmail.com>
2016-03-14 18:46:40 +0100

    First solution to hello-world

--- hello-world/babel-output/hello-world.js
+++ hello-world/babel-output/hello-world.js
@@ -1,34 +1,29 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+'use strict';
 
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-var HelloWorld = (function () {
-  function HelloWorld() {
-    _classCallCheck(this, HelloWorld);
+var _default = (function () {
+  function _default() {
+    _classCallCheck(this, _default);
   }
 
-  _createClass(HelloWorld, [{
-    key: "hello",
+  _createClass(_default, [{
+    key: 'hello',
     value: function hello() {
-      //
-      // YOUR CODE GOES HERE
-      //
+      var name = arguments.length <= 0 || arguments[0] === undefined ? 'World' : arguments[0];
+
+      return 'Hello, ' + name + '!';
     }
   }]);
 
-  return HelloWorld;
+  return _default;
 })();
 
-exports["default"] = HelloWorld;
-module.exports = exports["default"];
+exports['default'] = _default;
+module.exports = exports['default'];
