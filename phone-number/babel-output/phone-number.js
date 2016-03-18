1ab48149191e5d4153a5d8ffdb815de4c6df18b7
cebial <biglazypanda@gmail.com>
2016-03-18 12:16:49 +0100

    First solution to phone-number

---
+++ phone-number/babel-output/phone-number.js
@@ -0,0 +1,29 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+exports['default'] = function (number) {
+  return (function () {
+    var n = number.replace(/\D/g, '');
+    if (n.length === 11 && n[0] === '1') {
+      n = n.slice(1);
+    }
+    if (n.length !== 10) n = '0000000000';
+
+    return {
+      number: function number() {
+        return n;
+      },
+      areaCode: function areaCode() {
+        return n.slice(0, 3);
+      },
+      toString: function toString() {
+        return '(' + n.slice(0, 3) + ') ' + n.slice(3, 6) + '-' + n.slice(6);
+      }
+    };
+  })();
+};
+
+module.exports = exports['default'];
---
+++ phone-number/phone-number.js
@@ -0,0 +1,11 @@
+export default number => (() => {
+  let n = number.replace(/\D/g, '');
+  if (n.length === 11 && n[0] === '1') { n = n.slice(1); }
+  if (n.length !== 10) n = '0000000000';
+
+  return {
+    number: () => n,
+    areaCode: () => n.slice(0, 3),
+    toString: () => `(${n.slice(0, 3)}) ${n.slice(3, 6)}-${n.slice(6)}`
+  };
+})();
